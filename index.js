1c7a82f54567695f92554893f522fb986b857be2
claytonrod <clayton.rodrigues.c@gmail.com>
2023-04-01 10:33:36 -0300

    alter port

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const alunoRoute = require('./route/aluno')
 const cursoRoute = require('./route/curso')
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 const app = express()
 app.use(express.json())
 app.use(cursoRoute)
 app.use(alunoRoute)
 
-app.listen(3001,()=>{
-    console.log('rondando...')
+app.listen(PORT,()=>{
+    //console.log('rondando...porta ${PORT}')
+    console.log(`Rondando em .... port ${PORT}`);
 })
